3230fba951a477a85081c8905fed12fba71680d8
André Rodrigues <andre.rodrigues98@outlook.com>
2020-08-09 11:27:09 -0300

    inserindo adaptação para bancos de dados de teste

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import 'express-async-errors';
+import dotenv from 'dotenv';
 
 import routes from './routes';
 
 import errorsMiddleware from './middlewares/errors';
 import logResquest from './middlewares/logResquest';
 import createConnection from './database';
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env.dev',
+});
 
 createConnection();
 const app = express();
